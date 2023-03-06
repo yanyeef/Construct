51bfd676bed8fb952a04c537aba9426e5255fb18
Seth Vizel <sethv15@nycstudents.net>
2023-03-06 08:18:02 -0500

    c

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+class Main {
+    public static void main(String[] args)
+    {
+        String t1 = "This is test 1";
+        String2ArrayList st1 = new String2ArrayList();
+        st1.splitAndAdd(t1);
+        System.out.println(st1);
+    }
 }
--- src/String2ArrayList.java
+++ src/String2ArrayList.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class String2ArrayList {
+import java.util.ArrayList;
+public class String2ArrayList
+{
+    private ArrayList<String> splitString;
+
+    public String2ArrayList()
+    {
+        //Construct the splitString here!
+        splitString = new ArrayList<String>();
+    }
+    public void splitAndAdd(String str)
+    {
+        //split the string into single character strings 
+        //and add them ot splitString
+        for(int i = 0; i < str.length(); i++){
+            splitString.add(str.substring(i, i+1));
+        }
+    }
+    public String toString()
+    {
+        return splitString + "";
+    }
 }
